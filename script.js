a905d36902128e4560c8a18d6d5b6289684fa425
Josh Gulledge <joshcalebgulledge@gmail.com>
2021-01-28 14:09:17 -0600

    playing with styles

--- script.js
+++ script.js
@@ -4,4 +4,14 @@ $(document).ready(onReady);
 
 function onReady() {
   console.log('jQuery linked');
+
+  const h1 = $('h1');
+  console.log(h1);
+
+  const h2 = $('h2');
+  h2.css('background-color', 'blue');
+  $('h1').addClass('froggy');
+
+  const h1Text = $('h1');
+  h1Text.text('Goodbye');
 }
